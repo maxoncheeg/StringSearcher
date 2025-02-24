0b6d388096b12f99e9ac33c9386470be0a0943c7
maxoncheeg <maksgigks@outlook.com>
2025-02-24 14:19:11 +0700

    init

---
+++ Extensions/StringExtensions.cs
@@ -0,0 +1,66 @@
+﻿namespace SubstringSearcher.Extensions;
+
+public static class StringExtensions
+{
+    private const char SpecialSeparatorCharacter = '\0';
+
+    public static IList<int> Find(this string @this, string search)
+    {
+        var fullString = $"{search}{SpecialSeparatorCharacter}{@this}";
+        var prefixFunction = fullString.GetPrefixFunction();
+        IList<int> result = [];
+        
+        for (int i = search.Length + 1; i < fullString.Length; i++)
+            if(prefixFunction[i] == search.Length)
+                result.Add(i - search.Length * 2);
+        
+        return result;
+    }
+    
+    public static IList<int> FindSlow(this string @this, string search)
+    {
+        List<int> result = [];
+        
+        for (int i = 0; i < @this.Length; i++)
+        {
+            for (int j = 0; j < search.Length; j++)
+            {
+                if (i + j >= @this.Length) break;
+                if (@this[i + j] != search[j])
+                    break;
+                
+                if(j + 1 == search.Length)
+                    result.Add(i);
+            }
+        }
+        
+        return result;
+    }
+
+    public static IList<int> GetPrefixFunction(this string @this)
+    {
+        int k = 0;
+        IList<int> function = [0];
+
+        for (int i = 1; i < @this.Length; i++)
+        {
+            while (@this[i] != @this[k] && k != 0)
+                k = function[k - 1];
+
+            if (@this[i] == @this[k])
+                ++k;
+
+            function.Add(k);
+        }
+
+        return function;
+    }
+
+    public static string Multiply(this string @this, int multiplier)
+    {
+        var result = string.Empty;
+        for (var i = 0; i < multiplier; i++)
+            result += @this;
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using SubstringSearcher.Extensions;
+
+string text = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+//text = text[..^3];
+string search = "";//"ipsum dolor";
+int multiplier = 500;
+
+//Console.WriteLine($"TEXT: {text}");
+//Console.WriteLine($"SEARCH STRING: {search}");
+
+var watch = new System.Diagnostics.Stopwatch();
+IList<int> result = new List<int>();
+IList<int> result1 = new List<int>();
+
+text = text.Multiply(multiplier);
+
+search = text + "h";
+text = text + "haha";
+
+watch.Start();
+result = text.Find(search);
+watch.Stop();
+
+Console.WriteLine("\nTIME: " + watch.ElapsedMilliseconds);
+
+watch.Start();
+result1 = text.FindSlow(search);
+watch.Stop();
+
+if(result.Count != result1.Count)Console.WriteLine("FALSE FALSE FALSE");
+
+for (int i = 0; i < result.Count; i++)
+{
+    if (result[i] != result1[i])
+    {
+        Console.WriteLine("ERROR: FALSE FALSE FALSE");
+        break;
+    }
+}
+
+
+Console.WriteLine("\nTIME: " + watch.ElapsedMilliseconds);
